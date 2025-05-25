ff423c7439400037917266a9d32f101d4f000d73
tecnologia <tecnologia@grupo05.com.br>
2025-05-25 10:20:15 -0300

    alterações no vite para funcionamento em produção

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,20 +1,25 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     server: {
-        host: '0.0.0.0',
+        host: "0.0.0.0",
         port: 5173,
         hmr: {
-            host: 'localhost', // ou host.docker.internal no Mac/Windows, ou IP da sua máquina no Linux
+            host: "localhost", // ou host.docker.internal no Mac/Windows, ou IP da sua máquina no Linux
         },
-    },    
+    },
+    build: {
+        manifest: true, // Gera o manifest.json
+        outDir: "public/build", // Caminho de saída para os arquivos compilados
+    },
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/js/scroll.js',
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/bootstrap.js",
+                "resources/js/scroll.js",
             ],
             refresh: true,
         }),
