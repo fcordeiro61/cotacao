6d13786f5eccb89f25835210ea689a03d4fb9a42
tecnologia <tecnologia@grupo05.com.br>
2025-05-27 20:00:54 -0300

    Atualizacao do public_prod.sh

--- public_prod.sh
+++ public_prod.sh
@@ -9,8 +9,18 @@ echo "Iniciando publicação do branch prod..."
 git checkout prod
 git reset --hard master
 
+git rm -r --cached docker
+git rm -r --cached docker-compose.yml
+git rm -r --cached .env
+git rm -r --cached public_prod.sh
+
+
+git commit -m "Removendo arquivos desnecessários - docker"
 
 # Faz push forçado para o remoto
 git push origin prod --force
+
+# Limpa arquivos não rastreados para garantir que não reste nada no HEAD
+git clean -fd
 
 echo "Publicação concluída: branch prod está igual ao master."
